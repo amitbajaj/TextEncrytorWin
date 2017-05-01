0a87fd0b1c2e70dae1fa060f514f510044f101b8
Amit Bajaj <amitbajaj@windowslive.com>
2017-05-01 10:17:49 +0300

    Align with Java Code - 1.0

--- Encryption/Encryptor.cs
+++ Encryption/Encryptor.cs
@@ -11,97 +11,6 @@ namespace Encryption
     class Encryptor
     {
         private static int KEYLENGTH = 16;
-        public bool IsKeyValid(String key)
-        {
-            if (key.Length < 16)
-            {
-                return false;
-            }
-            if (key.Length % 8 != 0)
-            {
-                return false;
-            }
-            return true;
-        }
-        public byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
-        {
-            // Check arguments.
-            if (plainText == null || plainText.Length <= 0)
-                throw new ArgumentNullException("plainText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-            byte[] encrypted;
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for encryption.
-                using (MemoryStream msEncrypt = new MemoryStream())
-                {
-                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
-                        {
-                            //Write all data to the stream.
-                            swEncrypt.Write(plainText);
-                        }
-                        encrypted = msEncrypt.ToArray();
-                    }
-                }
-            }
-            // Return the encrypted bytes from the memory stream.
-            return encrypted;
-        }
-
-        public string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
-        {
-            // Check arguments.
-            if (cipherText == null || cipherText.Length <= 0)
-                throw new ArgumentNullException("cipherText");
-            if (Key == null || Key.Length <= 0)
-                throw new ArgumentNullException("Key");
-            if (IV == null || IV.Length <= 0)
-                throw new ArgumentNullException("IV");
-
-            // Declare the string used to hold
-            // the decrypted text.
-            string plaintext = null;
-
-            // Create an Aes object
-            // with the specified key and IV.
-            using (Aes aesAlg = Aes.Create())
-            {
-                aesAlg.Key = Key;
-                aesAlg.IV = IV;
-
-                // Create a decrytor to perform the stream transform.
-                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
-
-                // Create the streams used for decryption.
-                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
-                {
-                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
-                        {
-                            // Read the decrypted bytes from the decrypting stream
-                            // and place them in a string.
-                            plaintext = srDecrypt.ReadToEnd();
-                        }
-                    }
-                }
-            }
-            return plaintext;
-        }
-
         public byte[] EncryptStringToBytes_AesIV(string plainText, byte[] Key)
         {
             // Check arguments.
@@ -112,22 +21,13 @@ namespace Encryption
             byte[] encrypted;
             byte[] staged;
             byte[] key2use;
-            String prefix;
             // Create an Aes object
             // with the specified key and IV.
             using (Aes aesAlg = Aes.Create())
             {
                 key2use = new byte[KEYLENGTH];
-                if (Key.Length < KEYLENGTH)
-                {
-                    prefix = new String('0', KEYLENGTH - Key.Length);
-                    Buffer.BlockCopy(System.Text.Encoding.UTF8.GetBytes(prefix), 0, key2use, 0,prefix.Length);
-                    Buffer.BlockCopy(Key, 0, key2use, prefix.Length,Key.Length);
-                }
-                else
-                {
-                    Buffer.BlockCopy(Key, 0, key2use, 0, KEYLENGTH);
-                }
+                SHA256Managed sha256 = new SHA256Managed();
+                Buffer.BlockCopy(sha256.ComputeHash(Key), 0, key2use, 0, KEYLENGTH);
                 aesAlg.Key = key2use;
                 aesAlg.Mode = CipherMode.CBC;
                 aesAlg.Padding = PaddingMode.PKCS7;
@@ -176,16 +76,8 @@ namespace Encryption
             using (Aes aesAlg = Aes.Create())
             {
                 key2use = new byte[KEYLENGTH];
-                if (Key.Length < KEYLENGTH)
-                {
-                    prefix = new String('0', KEYLENGTH - Key.Length);
-                    Buffer.BlockCopy(System.Text.Encoding.UTF8.GetBytes(prefix), 0, key2use, 0, prefix.Length);
-                    Buffer.BlockCopy(Key, 0, key2use, prefix.Length, Key.Length);
-                }
-                else
-                {
-                    Buffer.BlockCopy(Key, 0, key2use, 0, KEYLENGTH);
-                }
+                SHA256Managed sha256 = new SHA256Managed();
+                Buffer.BlockCopy(sha256.ComputeHash(Key), 0, key2use, 0, KEYLENGTH);
                 aesAlg.Key = key2use;
                 Buffer.BlockCopy(cipherText, 0, iv, 0, 16);
                 Buffer.BlockCopy(cipherText, 16, staged, 0, cipherText.Length - 16);
